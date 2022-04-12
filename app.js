2440165bde804d8d35c8a914c9dad1e554952665
xdatalinq <89672040+xdatalinq@users.noreply.github.com>
2022-04-12 00:01:58 -0400

    generate webpage

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
-const printProfileData = profileDataArr => {
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
